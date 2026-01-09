374d0c013060c9c6b21c5ede7a8a6f77024d478f
Peme969 ⚡ <github@peme969.dev>
2026-01-09 12:28:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const themes = {
-  githubDark: {
+  standard: {
     background: "#101217",
-    text: "#ffffff",
-    meta: "#dddddd",
-    grade4: "#45bb3b",
-    grade3: "#27602a",
-    grade2: "#173b21",
-    grade1: "#102a1c",
-    grade0: "#16191f"
+    grade4: "#39d353",
+    grade3: "#26a641",
+    grade2: "#006d32",
+    grade1: "#0e4429",
+    grade0: "#151B23"
   },
   leftPad: {
     background: "#000000",
-    text: "#ffffff",
-    meta: "#999999",
     grade4: "#F6F6F6",
     grade3: "#DDDDDD",
     grade2: "#A5A5A5",
@@ -21,8 +17,6 @@ const themes = {
   },
   dracula: {
     background: "#181818",
-    text: "#f8f8f2",
-    meta: "#666666",
     grade4: "#ff79c6",
     grade3: "#bd93f9",
     grade2: "#6272a4",
@@ -31,8 +25,6 @@ const themes = {
   },
   blue: {
     background: "#181818",
-    text: "#C0C0C0",
-    meta: "#666666",
     grade4: "#4F83BF",
     grade3: "#416895",
     grade2: "#344E6C",
@@ -41,12 +33,29 @@ const themes = {
   },
   panda: {
     background: "#2B2C2F",
-    text: "#E6E6E6",
-    meta: "#676B79",
     grade4: "#FF4B82",
     grade3: "#19f9d8",
     grade2: "#6FC1FF",
     grade1: "#34353B",
     grade0: "#242526"
+  },
+  solarizedDark: {
+    background: "#002b36",
+    text: "#93a1a1",
+    meta: "#586e75",
+    grade4: "#d33682",
+    grade3: "#b58900",
+    grade2: "#2aa198",
+    grade1: "#268bd2",
+    grade0: "#073642"
   }
+};
+const availableThemes = {
+  standard: "GitHub Dark",
+  leftPad: "@left_pad",
+  dracula: "Dracula",
+  blue: "Blue",
+  panda: "Panda 🐼",
+  YlGnBu: "YlGnBu",
+  solarizedDark: 'Solarized Dark'
 };
